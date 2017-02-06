79883052d68db8d4414566cdb6221fb4394b6a92
pete <pete81833@gmail.com>
2017-02-06 17:14:36 +0800

    介面
    按鈕切換頁面成功

---
+++ app/src/main/java/com/example/arthur/chinesetree/PracticeActivity.java
@@ -0,0 +1,50 @@
+package com.example.arthur.chinesetree;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class PracticeActivity extends AppCompatActivity {
+
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+
+        context = this;
+
+        ImageView img0 = (ImageView)findViewById(R.id.img0);
+        img0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 產生一個Intent, 用來轉換Activity
+                Intent intent = new Intent();
+
+                // 設定轉換的Activity
+                intent.setClass(context, MainActivity.class);
+                // 轉換至下一個Activity
+                context.startActivity(intent);
+            }
+        });
+
+        ImageView img1 = (ImageView)findViewById(R.id.img1);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 產生一個Intent, 用來轉換Activity
+                Intent intent = new Intent();
+
+                // 設定轉換的Activity
+                intent.setClass(context, SearchActivity.class);
+                // 轉換至下一個Activity
+                context.startActivity(intent);
+            }
+        });
+    }
+
+}
+
